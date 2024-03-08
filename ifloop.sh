fc51e249651e03837f2ff8a4c7f3a9abd6c711d9
Indra kiran reddy Vaddeboyana <indrakiranreddyvaddeboyana@Indras-MacBook-Air.local>
2024-03-08 12:14:32 -0600

    ifloop

---
+++ ifloop.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+
+
+#######################
+#Author: Indra Kiran
+#Date: March 8th 2024
+#Version: V1
+# Simple ifloop
+
+
+
+
+
+
+a=4
+b=5
+
+if [ $a -gt $b ]; then
+	echo "a is greater than b"
+
+else
+	echo "b is greater than a"
+
+fi
